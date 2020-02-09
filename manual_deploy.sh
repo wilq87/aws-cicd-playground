67858fdc82b57aca79771d8f33b9c5d612fc9c8c
wilq87 <wilq87@gmail.com>
2020-02-09 17:26:23 +0100

    build with gradle

--- manual_deploy.sh
+++ manual_deploy.sh
@@ -3,6 +3,8 @@
 dt=$(date '+%Y%m%d-%H%M%S');
 echo "$dt"
 
+./gradlew clean build
+
 $(aws ecr get-login --no-include-email --region eu-west-1)
 docker build . --tag "awscicd-test"
 docker tag awscicd-test:latest 031709530795.dkr.ecr.eu-west-1.amazonaws.com/awscicd-test:latest
