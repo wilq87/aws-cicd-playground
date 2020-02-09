343a7e9f7bacc48cdf8772e0e995b4a69022b4db
wilq87 <wilq87@gmail.com>
2020-02-09 17:09:35 +0100

    deployment to ECR - manual using console - using buildspec.yml

---
+++ manual_deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dt=$(date '+%Y%m%d-%H%M%S');
+echo "$dt"
+
+$(aws ecr get-login --no-include-email --region eu-west-1)
+docker build . --tag "awscicd-test"
+docker tag awscicd-test:latest 031709530795.dkr.ecr.eu-west-1.amazonaws.com/awscicd-test:latest
+docker push 031709530795.dkr.ecr.eu-west-1.amazonaws.com/awscicd-test:latest
